d87ae973fe91cd2dc25461d407e2c89775da3078
unknown <zlovred12@gmail.com>
2023-02-12 02:28:36 +0300

    temp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+//        NumbersBeforeFour n = new NumbersBeforeFour();
+//        System.out.println(Arrays.toString(n.numbers(new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7})));
+//        System.out.println(Arrays.toString(n.numbers(new int[]{2, 2, 3, 1, 7})));
+//
+//
+//        OneAndFour of = new OneAndFour();
+//        System.out.println(of.oneFourNumbers(new int[]{1, 1, 1, 4, 4, 1, 4, 4 }));
+//        System.out.println(of.oneFourNumbers(new int[]{1, 1, 1, 1, 1, 1}));
+//        System.out.println(of.oneFourNumbers(new int[]{4,1}));
+//        System.out.println(of.oneFourNumbers(new int[]{4,4,4,4}));
+//        System.out.println(of.oneFourNumbers(new int[]{1, 4, 4, 1, 1, 4, 3}));
+    }
 }
--- src/test/java/NumbersBeforeFourTest.java
+++ src/test/java/NumbersBeforeFourTest.java
@@ -1,11 +1,17 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumbersBeforeFourTest {
+    private NumbersBeforeFour nf;
+
+    @Before
+    public void init(){
+        nf = new NumbersBeforeFour();
+    }
 
     @Test
     public void numberBeforeFour() {
-        NumbersBeforeFour nf = new NumbersBeforeFour();
         Assert.assertArrayEquals(new int[]{1, 7}, nf.numbers(new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7}));
     }
     @Test
@@ -23,6 +29,12 @@ public class NumbersBeforeFourTest {
     public void numberBeforeFour3() {
         NumbersBeforeFour nf = new NumbersBeforeFour();
         Assert.assertArrayEquals(new int[]{1, 7}, nf.numbers(new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7}));
+    }
+
+
+    @Test(expected = RuntimeException.class)
+    public void numberBeforeFourException() {
+        nf.numbers(new int[]{1, 2, 2, 3, 1, 7});
     }
 }
 
